88936e60e5a9b930b437b4ae4bf7c3a69b45d9c2
baskei0130 <keita.shimada.abc@keio.jp>
2021-09-21 11:36:54 +0900

    complete

---
+++ chap11/ex6/popcount.go
@@ -0,0 +1,50 @@
+package popcount
+
+// pc[i] は i のポピュレーションカウンタ
+var pc [256]byte
+
+func init() {
+	for i, _ := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+//PopCount は x のポピュレーションカウント (1 が設定されているビット数) を返す
+func PopCount1(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCount2(x uint64) int {
+	var pcOfx byte
+	for i := 0; i < 8; i++ {
+		pcOfx += pc[byte(x>>(uint(i)*8))]
+	}
+	return int(pcOfx)
+}
+
+func PopCount3(x uint64) int {
+	var count int
+	for i := 0; i < 64; i++ {
+		count += int((x >> uint64(i)) & uint64(1))
+	}
+	return count
+}
+
+func PopCount4(x uint64) int {
+	var count int
+
+	for x > 0 {
+		x &= (x - uint64(1))
+		//fmt.Print(x, "\n")
+		count++
+	}
+
+	return count
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/baskei0130/gopl.io
+
+go 1.15
+
+require (
+	github.com/apaxa-go/eval v0.0.0-20171223182326-1d18b251d679 // indirect
+	github.com/apaxa-go/helper v0.0.0-20180607175117-61d31b1c31c3 // indirect
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+)
